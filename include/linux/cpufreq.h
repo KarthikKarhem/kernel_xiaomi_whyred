2fc81c020cec0b56e21d15c9bcd70a3ea4bf204b
TripNRaVeR <noerialexeew@live.nl>
2017-11-05 19:45:31 +0100

    cpufreq: add tripndroid governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -555,6 +555,9 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
 extern struct cpufreq_governor cpufreq_gov_cultivation;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
+extern struct cpufreq_governor cpufreq_gov_tripndroid;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
